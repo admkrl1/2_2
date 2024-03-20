06d5e4d4b5b173b4f341532477993b5a7c125a21
admkrl1 <162460553+admkrl1@users.noreply.github.com>
2024-03-20 13:36:49 +0000

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,26 @@
 
 // import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
     Scanner scanner=new Scanner(System.in);
     int[] numbers=new int[5];
-    for(int i=0;i<5;i++)
-      System.out.println("Liczba"+)
+
+    System.out.println("Podaj 5 liczb:");
+    
+    for(int i=0;i<5;i++){
+      System.out.println("Liczba"+(i+1)+":");
+      numbers[i]=scanner.nextInt();
+    }
+    int min=numbers[0];
+    for (int i=1;i<5;i++){
+      if (numbers[i]<min){
+        min=numbers[i];
+      }
+    }
+    System.out.println("Najmniejsza liczba to:"+min);
   }
 
   // @Test
