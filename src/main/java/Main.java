298ab148f7caf1c839bcdc8f8b0c5402d972d436
admkrl1 <162460553+admkrl1@users.noreply.github.com>
2024-03-20 13:26:05 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner=new Scanner(System.in);
+    int[] numbers=new int[5];
+    for(int i=0;i<5;i++)
+      System.out.println("Liczba"+)
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
